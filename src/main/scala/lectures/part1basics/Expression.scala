18a603733ca38e835b5c3b5a481d49d4adf5a290
Aamir-rgb <aamir.y2k1@gmail.com>
2023-06-06 11:00:15 +0530

    6th June Changes

--- src/main/scala/lectures/part1basics/Expression.scala
+++ src/main/scala/lectures/part1basics/Expression.scala
@@ -2,22 +2,30 @@ package lectures.part1basics
 
 object Expression extends App {
 
-    val x = 1+2
+  //Expressions are evaluated to a value and they hve a type.Here 1+2 is an expression
     println(x)
 
+  //Operators + - * / & | ^ << >> >>>(right shift with zero extension)
   println(2+3*4)
 
+  // == != > >= < <=
   println(1 == x)
 
-  println(!(1 == x))
 
+  //! && ||
+  println(!(1 == x))
   var avariable = 2
+
+  //Also works with -= *= /= ..... side effects
   avariable +=3
   println(avariable)
+  val aCondition = true
 
   //Instruction -> Do Something Expression ->Give Value
 
-  val aCondition = true
+  //Everything in scala is an expression
+  //If in scala is an expression
+  val x = 1+2
   val aConditionedValue = if(aCondition) 5 else 3
   println(aConditionedValue)
 
---
+++ src/main/scala/part2/Objects.scala
@@ -0,0 +1,50 @@
+package part2
+
+object Objects {
+  //Scala does not have class level functionality
+  def main(args: Array[String]):Unit = {
+    val Linda = new Person("Linda")
+    val Jacob= new Person("Jacob")
+
+    //Scala Object = Singleton Instance
+    println(Linda == Jacob)
+
+  }
+  //object will not receive parameter
+  object Person {
+     val N_EYES = 2
+     def canFly: Boolean = false
+
+
+    def from(mother:Person,father:Person):Person = new Person("Bubble")
+    def apply(mother:Person,father:Person):Person = new Person("Bubble")
+
+
+    val Mary = Person
+    val John = Person
+
+    println(Mary == John)
+    println(Person.N_EYES)
+    println(Person.canFly)
+
+
+
+    //println(Linda == Jacob)
+
+//    val bubble = Person.from(Linda,Jacob)
+//    println(bubble)
+
+//    val bubbleRedefined = Person.apply(Linda,Jacob)
+//    println(bubbleRedefined)
+//
+//    println(bubble == bubbleRedefined)
+  }
+
+  class Person(val name:String) {
+
+  }
+
+  //Scala Object = Singleton Instance
+
+
+}
