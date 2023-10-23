b7a05b5a837e12cc686788c89b288a08f1faba89
Aamir-rgb <aamir.y2k1@gmail.com>
2023-10-23 11:06:18 +0530

    Operators

---
+++ src/main/scala/com/rockthejvm/part1basics/Recursion.scala
@@ -0,0 +1,62 @@
+package com.rockthejvm.part1basics
+
+import scala.annotation.tailrec
+
+object Recursion {
+
+  //Repetition = Recursion
+   def sumUntil(n:Integer):Integer = {
+     if(n <=0) return 0;
+     else  n +sumUntil(n-1)
+   }
+
+  def sumUntil_v2(n: Int): Int = {
+    @tailrec
+    def sumUntilTailrec(x: Int, accumulator: Int): Int =
+      if (x <= 0) accumulator
+      else sumUntilTailrec(x - 1, accumulator + x) // TAIL recursion = recursive call occurs LAST in its code path
+    sumUntilTailrec(n, 0)
+  }
+    def sumNumbersBet(a:Int,b:Int):Int = {
+      if(a > b) 0
+      else a+sumNumbersBet(a+1,b)
+    }
+  def sumNumbersBet_v2(a: Int,b:Int): Int = {
+    @tailrec
+    def sumNumbersBetTailrec(currentNumber: Int, accumulator: Int): Int =
+      if (currentNumber > b) accumulator
+      else sumNumbersBetTailrec(currentNumber+1, currentNumber+accumulator) // TAIL recursion = recursive call occurs LAST in its code path
+    sumNumbersBetTailrec(a, 0)
+  }
+
+    def main(args: Array[String]): Unit = {
+     println(sumUntil(2000))
+    println(sumUntil_v2(20000))
+      println(sumNumbersBet_v2(4,8))
+  }
+
+    //Exercises
+    //1 Concatenate a String n times
+    def stringConcat(n: Int,s:String): String = {
+      @tailrec
+      def stringTailrec(currentNumber: Int, accumulator: String): String =
+        if (currentNumber > n) accumulator
+        else stringTailrec(currentNumber-1, s+accumulator) // TAIL recursion = recursive call occurs LAST in its code path
+      stringTailrec(n, "")
+    }
+    //2 Make a fibonacci series using tail recursion
+    def fibonacci(n:Int):Int = {
+      @tailrec
+      def fibonacciTailRec(x:Int,accumulator:Int):Int =
+      var a = 0
+      var b = 1
+      var c = 0
+        c = a+ b
+        if( c > n ) accumulator
+        else fibonacciTailRec(x-1,c+accumulator)
+        fibonacciTailRec(n,0)
+    }
+
+
+
+}
